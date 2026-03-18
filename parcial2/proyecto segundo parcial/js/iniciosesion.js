fced11ff1553786604c0e0d6e9fb59463b73d983
Dios <hola.com>
2026-03-17 23:37:56 -0600

    quierounanovia

---
+++ parcial2/proyecto segundo parcial/js/iniciosesion.js
@@ -0,0 +1,32 @@
+const formulario = document.querySelector("#formulario");
+
+let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+
+formulario.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const datosFormulario = new FormData(formulario);
+    const datos = Object.fromEntries(datosFormulario.entries());
+
+    const usuarioEncontrado = usuarios.find((u) => u.correo === datos.correo);
+
+    if (!usuarioEncontrado) {
+        alert("Correo no registrado");
+        return;
+    }
+
+    if (datos.contraseña !== datos.verificar) {
+        alert("Las contraseñas no coinciden");
+        return;
+    }
+
+    if (usuarioEncontrado.contraseña !== datos.contraseña) {
+        alert("La contraseña es incorrecta");
+        return;
+    }
+
+    alert("Inicio de sesión exitoso");
+
+    window.location.href = "../paginas/paginaprincipal.html";
+});
